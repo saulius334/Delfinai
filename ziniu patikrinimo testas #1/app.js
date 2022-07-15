00ac122424fe37679e8906b74c65502c40632e61
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-07-15 12:43:52 +0300

    5 semi-done

--- ziniu patikrinimo testas #1/app.js
+++ ziniu patikrinimo testas #1/app.js
@@ -8,7 +8,7 @@ const girls = ['Brigita', 'Edita', 'Deimantė', 'Justė', 'Ona', 'Bronė', 'Ramu
 // Sukurkite masyvą cats iš 10 elementų, kurių reikšmės atsitiktiniai skaičiai nuo 2 iki 12;
 // Sukurkite masyvą owners iš 10 elementų, kurių reikšmės atsitiktiniai vardai iš masyvo girls. 
 // Vardai gali kartotis. Masyvus atspausdinkite su console.log();
-
+console.log(`--------1`);
 let cats = [...Array(10)].map(_ => random(2, 12))
 console.log(cats);
 let owners = [...Array(10)].map(_ => girls[random(0, girls.length - 1)])
@@ -18,13 +18,14 @@ console.log(owners);
 // Suskaičiuokite kiek katinukų yra cats masyve (suraskite masyvo narių sumą).
 // Sumuokite tik tas katinukų reikšmes, kurios be liekanos dalijasi iš 3.
 // Rezultatą atspausdinkite su console.log();
-
+console.log(`--------2`);
 let dalijasi = cats.filter(a => (a / 3) === ((a / 3) - (a / 3 % 1)))
 console.log(dalijasi.reduce((a,b) => a + b));
 
 //3
 // Į masyvo owners pradžią, t.y. elementą su indeksu 0 (o ne galą!, o pridėkite papildomai, o neužrašykite ant mergaitės!)
 // pridėkite vardą 'Nausėda'. Masyvas po pridėjimo turi turėti 11 elementų;
+console.log(`--------3`);
 owners.unshift(`Nausėda`)
 //4
 // Sukurkite trečią masyvą catOwners, kurio reikšmė yra stringas, 
@@ -33,10 +34,25 @@ owners.unshift(`Nausėda`)
 // pirmai mergaitei iš owners masyvo priskiriamas pirmas katinukų skaičius iš cats masyvo,antrai - antras ir t.t. 
 // Atkreipkite dėmesį, kad pirma mergaitė owners masyve yra ne pirmas elementas su indeksu 0 (pirmas yra 'Nausėda'),
 // o antrasis su indeksu 1. Pirmas katinukų skaičius cats masyve yra standartiškai su indeksu 0. Masyvą atspausdinkite su console.log();
-let catOwners = owners.map((a, i) => `${a} has ${cats[i]} cats`)
+console.log(`--------4`);
+let catOwners = owners.map((a, i, t) => `${t[i + 1]} has ${cats[i]} cats`).slice(0, -1)
 console.log(catOwners);
 
 //5
 // Suraskite vieną (tik vieną) vardą owners masyve,
 // kuris kartojasi (nesvarbu kiek kartų) ir jį atspausdinkite su console.log().
-owners.forEach(a => a === owners.forEach(a => a) ? console.log(a) : `nera`)
+console.log(`--------5`);
+// blet tokia nesamone kad nu na
+let found = false
+for (let i = 0; i < owners.length; i++) {
+ for (let j = i + 1; j < owners.length;j++) {
+  if (owners[i] === owners[j]) {
+    console.log(owners[i])
+    found = true
+    break;
+  }
+ }
+ if (found === true) {
+  break;
+ }
+ }
