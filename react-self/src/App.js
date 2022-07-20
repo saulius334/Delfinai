85573cab766ff7799784b3603e46668f31dea7e8
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-07-20 12:08:48 +0300

    initial commit

---
+++ react-self/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState} from 'react';
+import TodoList from './TodoList'
+
+function App() {
+  const [todos, setTodos] = useState([`Todo 1`, `Todo 2`])
+  return (
+    <>
+      <TodoList todos={todos}/>
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
