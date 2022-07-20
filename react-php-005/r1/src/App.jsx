adfa4aeb812715485da7648dee51f7a26e5f0814
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-07-20 13:57:04 +0300

    commit

---
+++ react-php-005/r1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Kosmosas from './Components/005/Kosmosas';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Kosmosas></Kosmosas>
+      </header>
+    </div>
+  );
+}
+
+export default App;
