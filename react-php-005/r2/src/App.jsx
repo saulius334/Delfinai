ce826a75300be472f9e593a90712daf65ede6733
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-07-27 14:35:33 +0300

    CRUD create almost done

--- react-php-005/r2/src/App.jsx
+++ react-php-005/r2/src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css';
+import './App.scss';
 import './bootstrap.css';
+import AnimalsContext from './components/AnimalsOne';
 import Create from './components/Create';
 
+  const animalsTypes = [
+    {id:1, type: `antis`},
+    {id:2, type: `avis`},
+    {id:3, type: `antilope`},
+  ]
 
 function App() {
   return (
     <>
+    <AnimalsContext.Provider value={{
+      animalsTypes
+    }}>
     <div className="container">
 
 <div className="row">
@@ -18,6 +27,7 @@ function App() {
   </div>
 </div>
 </div>
+</AnimalsContext.Provider>
     </>
   );
 }
--- react-php-005/r2/src/components/Create.jsx
+++ react-php-005/r2/src/components/Create.jsx
@@ -1,9 +1,39 @@
-function Create() {
+import { useState } from "react";
+import { useContext } from "react";
+import AnimalsContext from "./AnimalsOne";
 
-    return (
-        <>
-        creat
-        </>
-    )
+function Create() {
+  const { animalsTypes } = useContext(AnimalsContext);
+  const [type, setType] = useState(1);
+  const [svoris,setSvori] = useState(``)
+  return (
+    <>
+      <div className="card m-4">
+        <h5 className="card-header">Add new animal</h5>
+        <div className="card-body">
+          <div className="form-group">
+            <label>Animal selection</label>
+            <select
+              className="form-control"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              {animalsTypes.map((a) => (
+                <option key={a.id} value={a.id}>
+                  {a.type}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Kiek sveria {animalsTypes.find(a => a.id == type).type}?</label>
+            <input type="text" className="form-control" value={svoris}  onChange={e => setSvori(e.target.value)}/>
+            <small className="form-text text-muted">Please enter your animal weigth</small>
+          </div>
+          <button type="button" className="btn btn-outline-info m-2">Add animal</button>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Create
+export default Create;
