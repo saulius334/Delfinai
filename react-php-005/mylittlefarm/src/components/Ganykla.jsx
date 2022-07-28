48a8360003da9cd0a36e37e2272438457e598703
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-07-28 10:00:07 +0300

    All-in-one

--- react-php-005/mylittlefarm/src/components/Ganykla.jsx
+++ react-php-005/mylittlefarm/src/components/Ganykla.jsx
@@ -1,39 +1,54 @@
 import { useState } from "react"
-import Karvesmaker from "./Karvesmaker"
-import Avysmaker from "./Avysmaker"
+import Random from "../functions/random"
+import RandomSerialnm from "./RandomSerialnm"
 
 function Ganykla() {
-
+    const stylekarve = {
+        width: `80px`,
+        height: `80px`,
+        backgroundColor:`Brown`,
+        cursor: `pointer`,
+        margin: `10px`,
+    }
+    const styleavis = {
+        width: `80px`,
+        height: `80px`,
+        backgroundColor:`Brown`,
+        cursor: `pointer`,
+        margin: `10px`,
+    }
+    const pstyle = {
+        fontSize: `16px`
+    }
     const [avys, setAvys] = useState([])
     const [karves,setKarves] = useState([])
     const iganykla = () => {
         setKarves(a => {
             a = [];
-             return [...a,<Karvesmaker onClick={switcheroo}/>]
+             return [...a,...Array(Random(5,20))]
         })
         setAvys(a => {
             a = [];
-             return [...a,<Avysmaker/>]
+             return [...a,...Array(Random(5,20))]
         })
     }
     const switcheroo = () => {
-        
+        console.log(`labas`);
     }
-
     return (
         <>
         <fieldset className="ganyklafield">
             <legend>Ganykla</legend>
         <button className="ganyklabtn" onClick={iganykla}>I GANYKLA</button>
         <div className="Ganykla">
-        <div className="Karves">
-        {
-            avys.map(a => a)
-        }
-        </div>
         <div className="Avys">
         {
-            karves.map(a => a)
+            avys.map((a,i) => <div onClick={switcheroo} style={styleavis} key={i}><p style={pstyle}>Avis A{RandomSerialnm()}</p></div>)
+        }
+        </div>
+        <div className="Karves">
+        {
+            karves.map((a,i) => <div onClick={switcheroo} style={stylekarve} key={i}><p style={pstyle}>Karve K{RandomSerialnm()}</p></div>)
         }
         </div>
         </div>
