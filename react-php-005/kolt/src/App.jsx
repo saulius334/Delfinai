f91187c54ec7f8a7e3f1aa73b7c4d47fffbc8340
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-07-29 14:33:10 +0300

    update

--- react-php-005/kolt/src/App.jsx
+++ react-php-005/kolt/src/App.jsx
@@ -1,66 +1,90 @@
-import { useState } from 'react';
-import './App.css';
-import KoltContext from './components/KoltContext';
-import Paspirdukas from './components/Paspirdukas';
+import { useEffect, useState } from "react";
+import "./App.css";
+import KoltContext from "./components/KoltContext";
+import { Create, Read, Update, Destroy } from "./functions/localstorage";
+import { useContext } from "react";
+import Paspirdukas from "./components/Paspirdukas";
+import RndSerial from "./functions/Rndserial";
+// import KoltContext from "./components/KoltContext"
+const key = `KOLTAS`;
+
+// function Paspirdukas() {
+//   const { nRida } = useContext(KoltContext)
+//   console.log({Serialnr: RndSerial(), isBusy: false, Rida: parseFloat(nRida)})
+//   return {Serialnr: RndSerial(), isBusy: false, Rida: parseFloat(nRida)}
+
+// }
 
 function App() {
-  
-
-  const [paspirdukuSarasas, setPaspirdukuSarasas] = useState([])
-  const [nRida, setnRida] = useState(``)
+  const [paspirdukuSarasas, setPaspirdukuSarasas] = useState(null);
+  const [nRida, setnRida] = useState(0);
   const irasytarida = (e) => {
-    setnRida(e.target.value)
-  }
+    setnRida(e.target.value);
+  };
   const pridetiPaspirduka = () => {
-    setPaspirdukuSarasas(a => [...a, <Paspirdukas irasytarida={nRida}/>])
-  }
 
+    setPaspirdukuSarasas(a => [...a,{paspirtukas: `geras`}])
+    Create(key, {paspirtukas: `geras`})
+  };
+  useEffect(() => {
+  setPaspirdukuSarasas(Read(key))
+  }, []);
 
+  // useEffect(() => {
+  //   console.log(paspirdukuSarasas)
+  //   if (paspirdukuSarasas === null) {
+  //     return;
+  //   }
+  //   localStorage.setItem(key, JSON.stringify(paspirdukuSarasas));
+  // }, [paspirdukuSarasas]);
 
   return (
-    <KoltContext.Provider value={{
+    <KoltContext.Provider
+      value={{
+        nRida,
+      }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <fieldset className="Pagrindinis">
+            <legend>Kolt paspituku nuoma</legend>
 
-    }}>
-    <div className="App">
-      <header className="App-header">
-        <fieldset className='Pagrindinis'>
-          <legend>Kolt paspituku nuoma</legend>
-          <div className="container">
-            <fieldset>
-              <legend>Uzregistruoti nauja</legend>
-              <label>Paspirtuko rida:</label>
-              <input type="text" onChange={irasytarida}></input>
-          <button className='btn' onClick={pridetiPaspirduka}>Atsivesti nauja paspirduka</button>
-          </fieldset>
-          <fieldset style={{width:`70%`}}>
-            <legend>Statistika</legend>
-            <p>Is viso paspirtuku:</p>
-            {/* {paspirtukuKiekis} */}
-            <p>Bendrai nuvaziuoti km:</p>
-            {/* {nuvaziuotaBendraiKM} */}
-          </fieldset>
-          </div>
-          <div className="container PaspirdukuSarasas">
-            <div className='ieskotiContainer'>
-            <p className='ieskoti'>Ieskoti pagal koda:</p>
-            <input type="text"></input>
+            <div className="container">
+              <fieldset>
+                <legend>Uzregistruoti nauja</legend>
+                <label>Paspirtuko rida:</label>
+                <input type="text" onChange={irasytarida} value={nRida}></input>
+                <button className="btn" onClick={pridetiPaspirduka}>
+                  Atsivesti nauja paspirduka
+                </button>
+              </fieldset>
+              <fieldset style={{ width: `70%` }}>
+                <legend>Statistika</legend>
+                <p>Is viso paspirtuku:</p>
+                {/* {paspirtukuKiekis} */}
+                <p>Bendrai nuvaziuoti km:</p>
+                {/* {nuvaziuotaBendraiKM} */}
+              </fieldset>
             </div>
-          {
-            paspirdukuSarasas.map((a,i) =>
-            <div className='paspirdukas' key={i}>
-              <p>{a.isBusy}</p>
-              <div className='updateButtonsDiv'>
-              <button className='btn update'>Trinti</button>
-              <button className='btn update'>Redaguoti</button>
+            <div className="container PaspirdukuSarasas">
+              <div className="ieskotiContainer">
+                <p className="ieskoti">Ieskoti pagal koda:</p>
+                <input type="text"></input>
               </div>
-              </div>)
-          }
-          </div>
-
-
-        </fieldset>
-      </header>
-    </div>
+              {console.log(paspirdukuSarasas)}
+              {paspirdukuSarasas?.map((a, i) => (
+                <div className="paspirdukas" key={i}>
+                  {/* <Paspirdukas /> */}sffsf
+                  <div className="updateButtonsDiv">
+                    <button className="btn update">Trinti</button>
+                    <button className="btn update">Redaguoti</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </fieldset>
+        </header>
+      </div>
     </KoltContext.Provider>
   );
 }
